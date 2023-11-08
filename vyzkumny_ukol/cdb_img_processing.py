3b8e03263fde4589b013ea023dee8262e0900754
Alex-JHUB-VPN <bogdaale@fjfi.cvut.cz>
2023-11-08 11:56:48 +0100

    New JN with ResNet pretrained model.

--- vyzkumny_ukol/cdb_img_processing.py
+++ vyzkumny_ukol/cdb_img_processing.py
@@ -5,7 +5,7 @@ import numpy as np         # práce s numerickými poli
 from pathlib import Path   # reprezentace cest v souborovém systému
 import xarray as xr        # data s osami
 from PIL import Image
-
+from tqdm.auto import tqdm
 
 def load_RIS_data(shot: int, ris: int) -> xr.DataArray:
     """Load RAW camera data from database
@@ -49,16 +49,20 @@ def demosaic(data: xr.DataArray) -> np.array:
     xar=xr.DataArray([r,g,b], dims=('color', 'time', 'x', 'y'), coords={'color': ['r','g','b'], 'time': data.coords['time'],\
                                                                          'x': np.arange(r.shape[1]), 'y':np.arange(r.shape[2])})
     axr=xar.transpose('time', 'x', 'y', 'color')
-    np_axr = axr.data.astype('float64') 
 
-    #Normalizace
-    channel_min = np.min(np_axr, axis=(0, 1, 2))  # minimum value for each time and channel
-    channel_span = np.max(np_axr, axis=(0, 1, 2)) - channel_min  # span of values for each time and channel
-    channel_norm = np.where(channel_span > 0, channel_span, 1)  # use 1 when span is 0
-    axr = (axr - channel_min[None, None, :]) / channel_norm[None, None, :]
-    np_axr=axr.data
+    # kazdy pixel ma 12 bitu (hodnoty 0..4096) => normalizujeme na rozsah 0..1
+    axr=axr/(2**12-1)  #Nejde tu pouzit axr/=... "Cannot cast ufunc 'divide' output from dtype('float64') to dtype('uint16') with casting rule 'same_kind'"
 
-    return np_axr, axr
+    #Normalizace - stara
+    #np_axr = axr.data.astype('float64') 
+    #channel_min = np.min(np_axr, axis=(0, 1, 2))  # minimum value for each time and channel
+    #channel_span = np.max(np_axr, axis=(0, 1, 2)) - channel_min  # span of values for each time and channel
+    #channel_norm = np.where(channel_span > 0, channel_span, 1)  # use 1 when span is 0
+    #axr = (axr - channel_min[None, None, :]) / channel_norm[None, None, :]
+    #np_axr=axr.data
+
+
+    return axr
 
 def flip_image(data: xr.DataArray, flip_horizontal: bool = True, flip_vertical: bool = True) -> np.array:
     """Flip image data vertically and horizontally.
@@ -113,9 +117,8 @@ def discharge_duration(shot: int, threshold: float = 1e4) -> Tuple[float, float]
         Tuple[plasma_start, plasma_end]
     """
 
-    # Note: the data are huge ==> caching them for future use would soon deplete our memory => cache=False
-    s = cdbxr.Shot(shot, cache=False) #load the whole shot
-    ipla = s[f"I_plasma:{shot}"] #load current
+    s = cdbxr.Shot(shot) #load the whole shot
+    ipla = s["I_plasma"] #load current
     plasma_time = ipla[abs(ipla)>threshold].time.data
     start = plasma_time[0]
     end = plasma_time[-1]
@@ -135,79 +138,19 @@ def save_ris_images_to_folder(data: int, shot, path: Path, ris: int, use_dischar
     Returns:
         filenames: np.array with all the names of saved images
     """
-    filenames = np.array([]) #str array with all the names of saved images
+    filenames = [] #str array with all the names of saved images
 
     if use_discharge_duration:
         start, end = discharge_duration(shot, 1e4)
-        dem_data=flip_image(demosaic(data)[1].sel(time=slice(start,end)))
+        dem_data=flip_image(demosaic(data).sel(time=slice(start,end)))
 
     else:
-        dem_data=flip_image(demosaic(data)[1])
-    
-    for i,frame in enumerate(dem_data):
+        dem_data=flip_image(demosaic(data))
+    for frame in tqdm(dem_data, total=len(dem_data)):    # automaticky se zobrazi a bude v prubehu cyklu updatovat progressbar
+    #for i,frame in enumerate(dem_data):
         filename = save_frame(path=path,frame=frame,ris=ris, shot=shot, time=frame.time.data)
-        filenames = np.append(filenames,filename)
-        if i%50==0:
-            print(f'{i}/{len(dem_data)} imgs are saved')
+        filenames.append(filename)
+        #if i%50==0:
+            #print(f'{i}/{len(dem_data)} imgs are saved')
     return filenames
 
-
-def heat_flux_datasets(scaled_Qdata, test_split, train_split, dropouts, target_fields,):
-    
-    # Drop desired features
-    for col in scaled_Qdata.columns:
-        for feature in dropouts:
-            if (col[:len(feature)]==feature):
-                print(f'Dropping {col}')
-                scaled_Qdata = scaled_Qdata.drop([col], axis=1)
-
-    # Create randomized data set.
-    data_set = scaled_Qdata.sample(n=scaled_Qdata.shape[0])
-
-    ### Split the data to test, training, and validation sets
-
-    # Define offsets
-    data_set_size = data_set.shape[0]
-
-    # use given % of all data for testing
-    test_set_size = int(data_set_size * test_split)
-
-    # use given % of training data for validation
-    train_set_size = int((data_set_size - test_set_size) * train_split)
-    valid_set_size = (data_set_size - test_set_size) - train_set_size
-
-    # Split data
-    test_set = data_set.iloc[:test_set_size]
-    train_set = data_set.iloc[test_set_size:test_set_size+train_set_size]
-    validation_set = data_set.iloc[test_set_size+train_set_size:]
-
-    # Keep ordered data for visualization (contains test, training, and validation sets)
-    vis_set = scaled_Qdata # All data (not randomized) is kept for visualization of model
-
-    # Split sets to features and targets subsets
-    test_features, test_targets =\
-        test_set.drop(target_fields, axis=1), test_set[target_fields]
-    train_features, train_targets =\
-        train_set.drop(target_fields, axis=1), train_set[target_fields]
-    validation_features, validation_targets =\
-        validation_set.drop(target_fields, axis=1), validation_set[target_fields]
-    # Special object for visualization
-    vis_features, vis_targets =\
-        vis_set.drop(target_fields, axis=1), vis_set[target_fields]
-
-    # Create pytorch tensor format data sets
-    test_set =\
-        TensorDataset(torch.tensor(test_features.values).float(),\
-                      torch.tensor(test_targets[target_fields].values).float())
-    train_set =\
-        TensorDataset(torch.tensor(train_features.values).float(),\
-                      torch.tensor(train_targets[target_fields].values).float())
-    validation_set =\
-        TensorDataset(torch.tensor(validation_features.values).float(),\
-                      torch.tensor(validation_targets[target_fields].values).float())
-    # Special data set for visualization (ordered)
-    vis_set =\
-        TensorDataset(torch.tensor(vis_features.values).float(),\
-                      torch.tensor(vis_targets[target_fields].values).float())
-    
-    return test_set, train_set, validation_set, vis_set
